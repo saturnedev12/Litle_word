5eab940946f0a93113b95443f5dcf379fed44f1b
saturnedev12 <saturne.dev12@gmail.com>
2020-07-20 17:43:27 +0000

    retouche mineur du modal d'enregistrement , sécurisation des donnéze entré dans la base de donnée au niveau du back-end , creation de certaine fonctions au niveau du fichier web et ajout d'une icone dans le titre

--- routes/web.php
+++ routes/web.php
@@ -1,23 +1,45 @@
 <?php
 use Illuminate\Support\Facades\Route;
+//donnnee necessaire a un article
+function getArticleData(){
+  return DB::select('SELECT owner.name, card.title, card.texte, card.carte_color, card.texte_color FROM owner, card WHERE card.id_owner = owner.ID');
+}
+//verification des donnnee
+function verifie($tb = array()){
+	$etat;
+ foreach($tb as $data){
+	if(!empty($data) && isset($data)){
+		$etat = true;
+       }
+	else{
+		$etat= false;
+		break;
+	 }
+	}
+	return $etat;
+}
 
+//::::LES ROUTES::::
 Route::get('/', function () {
-  $data = DB::select('SELECT owner.name, card.title, card.texte, card.carte_color, card.texte_color FROM owner, card WHERE card.id_owner = owner.ID');
-    return view('welcome')->with('data',$data);
+    return view('welcome')->with('data',getArticleData());
 });
 
 Route::post('/', function () {
-     $id_owner = DB::select("SELECT id FROM owner WHERE name=? ",[$_POST['owner']]);
-     $val =[
-       $_POST['title'],
-       $id_owner[0]->id,
-       $_POST['content'],
-       $_POST['carte_color'],
-       $_POST['texte_color']
-     ];
-     //dd($val);
-     DB::insert("INSERT INTO card(title, id_owner, texte, carte_color, texte_color) VALUES(?,?,?,?,?)",$val);
 
-    $data = DB::select('SELECT owner.name, card.title, card.texte, card.carte_color, card.texte_color FROM owner, card WHERE card.id_owner = owner.ID');
-    return view('welcome')->with('data',$data);
+     $id_owner = DB::select("SELECT id FROM owner WHERE name=? ",[$_POST['owner']]);
+     $data =[
+       'title'=> $_POST['title'],
+        'id_owner'=> $id_owner[0]->id,
+        'texte'=>$_POST['content'],
+        'carte_color'=>$_POST['carte_color'],
+        'texte_color'=>$_POST['texte_color']
+     ];
+    if(verifie($data)){
+     DB::table('card')->insert($data);
+   }
+   else{
+    echo"<script>alert('donnée invalide');</script>";
+   }
+
+    return view('welcome')->with('data',getArticleData());
 });
